543b8654fa722077d5af58c131845367af92b749
Amardeep Ganguly <83149058+punyprogrammer@users.noreply.github.com>
2021-10-23 20:45:51 +0530

    Create background.js
    Added background script to filter out google ads

---
+++ src/background.js
@@ -0,0 +1,15 @@
+chrome.webRequest.onBeforeRequest.addListener(
+function(details) {
+const url =details.url
+const filters=['googleadservices','googlesyndication','g.doubleclick']
+for(const filter of filters)
+{
+if(url.indexOf(filter)!=-1)
+{return  {cancel:true}}
+}
+return  {cancel:false}},
+
+{
+urls:["<all_urls>"]
+},["blocking"]
+)
